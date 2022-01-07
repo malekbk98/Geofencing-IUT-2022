837d1505c21f5a2ec223ece1a9a8fd5126782166
Malek BK <malekbk98@gmail.com>
2022-01-07 17:35:37 +0100

    create navigation

---
+++ lib/pages/zones.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:geofencing/widget/navigation_drawer_widget.dart';
+
+class ZonesPage extends StatelessWidget{
+  @override
+  Widget build (BuildContext context) => Scaffold(
+    endDrawer: NavigationDrawerWidget(),
+    appBar: AppBar(
+      title: Text('Les zones'),
+      centerTitle: true,
+      backgroundColor: Color.fromRGBO(34, 36, 43, 1.0),
+    ),
+  );
+}
