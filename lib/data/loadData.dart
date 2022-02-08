bdb0b1fadd37869f1b091941b1c1bb745dd1feaf
Malek BK <malekbk98@gmail.com>
2022-02-08 15:09:21 +0100

    Map view & current location

---
+++ lib/data/loadData.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:geofencing/models/Zone.dart';
+import 'package:http/http.dart' as http;
+
+late List<Zone> zones;
+Zone? mainZone = null;
+
+//Fetch main zone
+Future<Zone> fetchMainZone() async {
+  final response = await http.get(
+      Uri.parse('http://docketu.iutnc.univ-lorraine.fr:62004/items/terrain'));
+
+  if (response.statusCode == 200) {
+    //Save result (need to be stored in cache later)
+    var data = jsonDecode(response.body)['data'][0];
+    return Zone(
+      id: data['id'],
+      status: data['status'],
+      nom: data['nom'],
+      description: data['description'],
+      coordonnees: data['coordonnees']['coordinates'][0],
+    );
+  } else {
+    throw Exception('Failed to load main zone');
+  }
+}
+
+//Fetch all zones
+Future<List<Zone>> fetchZones() async {
+  final response = await http
+      .get(Uri.parse('http://docketu.iutnc.univ-lorraine.fr:62004/items/zone'));
+  if (response.statusCode == 200) {
+    //Save result (need to be stored in cache later)
+    List<Zone> tempZones = [];
+    var data = jsonDecode(response.body)['data'];
+    for (var zone in data) {
+      tempZones.add(Zone(
+        id: zone['id'],
+        status: zone['status'],
+        nom: zone['nom'],
+        description: zone['description'],
+        coordonnees: zone['coordonnees']['coordinates'][0],
+      ));
+    }
+    return tempZones;
+  } else {
+    throw Exception('Failed to load zones');
+  }
+}
+
+initData() {
+  //Load main zone
+  try {
+    fetchMainZone();
+  } catch (e) {
+    print(e.toString());
+  }
+
+  //Load all zones
+  try {
+    fetchZones();
+  } catch (e) {
+    print(e.toString());
+  }
+}
--- lib/models/Zone.dart
+++ lib/models/Zone.dart
@@ -1,16 +1,24 @@
 class Zone {
   final int id;
+  final String status;
+  final String nom;
   final String description;
   final Object coordonnees;
 
 //Constructor
   const Zone(
-      {required this.id, required this.description, required this.coordonnees});
+      {required this.id,
+      required this.status,
+      required this.nom,
+      required this.description,
+      required this.coordonnees});
 
   //From Json
   factory Zone.fromJson(Map<String, dynamic> json) {
     return Zone(
       id: json['id'],
+      status: json['status'],
+      nom: json['nom'],
       description: json['description'],
       coordonnees: json['coordonnees'],
     );
