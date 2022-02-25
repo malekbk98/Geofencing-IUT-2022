dc465bd85ec65d78aeb5c8ddc69d61e4331cdcb9
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-25 12:05:01 +0100

    add check update

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:geofencing/widgets/navigation_drawer_widget.dart';
 import 'package:geofencing/theme/app_theme.dart';
 
+import 'package:geofencing/data/loadData.dart';
+
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
@@ -24,12 +26,16 @@ class _SettingScreenState extends State<SettingScreen> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
+                Image.asset(
+                  'assets/logo.png',
+                  scale: 3,
+                ),
                 SwitchListTile(
                   secondary: _notifications
                       ? const Icon(Icons.notifications_active)
                       : const Icon(Icons.notifications_off),
                   title: Text(
-                    'Enable/disbale notifications',
+                    'Activez/désactiver les notifications',
                     style: TextStyle(
                       color: AppTheme.mainColor,
                     ),
@@ -40,6 +46,31 @@ class _SettingScreenState extends State<SettingScreen> {
                       _notifications = value;
                     });
                   },
+                ),
+                Column(
+                  children: [
+                    ElevatedButton.icon(
+                      label: const Text('Vérifiez les mises à jour'),
+                      icon: const Icon(Icons.update),
+                      onPressed: () async {
+                        bool updateCheck = await fetchIdUpdate();
+                        if (updateCheck) {
+                          initData();
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text("Mises à jour des données terminée"),
+                          ));
+                        }
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Aucune mises à jour est disponible"),
+                        ));
+                      },
+                      style:
+                          ElevatedButton.styleFrom(primary: AppTheme.mainColor),
+                    ),
+                    const Text('Dernière date de mises à jour : DD-MM-YYYY')
+                  ],
                 ),
               ],
             ),
